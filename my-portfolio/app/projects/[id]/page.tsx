4c6ac2da91c1afd59e5ee64cd240718c2f38b2a5
SzewczykSzy <szewczyk.szy@gmail.com>
2024-07-16 12:46:56 +0200

    Video/gif is displayed

--- my-portfolio/app/projects/[id]/page.tsx
+++ my-portfolio/app/projects/[id]/page.tsx
@@ -1,5 +1,3 @@
-// Ensure to add a dark-mode class to the body element or a top-level wrapper element when toggling dark mode
-
 "use client";
 
 import { useParams } from 'next/navigation';
@@ -15,12 +13,14 @@ const ProjectDetailsPage = () => {
     return <div>Project not found</div>;
   }
 
+  const isVideo = project.finalMedia.src.endsWith('.mp4');
+
   return (
     <div className="container mx-auto pt-5 text-center max-w-screen-lg px-8">
       <h1 className="fw-light pb-5">{project.title}</h1>
       {project.details.map((detail, index) => (
         <div key={index} className="clearfix mb-5">
-          <div className={`float-${index % 2 === 0 ? 'end' : 'start'} mb-3 ms-md-3 detailed-image`}>
+          <div className={`float-${index % 2 === 0 ? 'end' : 'start'} mb-3 ms-md-3`}>
             <Image
               src={detail.image}
               alt={`Detail ${index + 1}`}
@@ -34,28 +34,28 @@ const ProjectDetailsPage = () => {
       ))}
       <div className="video-container">
         <h4>{project.finalMedia.description}</h4>
-        {project.finalMedia.type === 'video' ? (
-          <video controls className="video-content">
+        {isVideo ? (
+          <video controls className="w-full mt-4" style={{ maxWidth: '800px', maxHeight: '400px' }}>
             <source src={project.finalMedia.src} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         ) : (
-          <Image
-            src={project.finalMedia.src}
-            alt="Final media"
-            className="img-fluid mb-3 px-5 rounded video-content"
-            width={800}
-            height={400}
-            style={{ objectFit: 'contain', width: '100%', height: '100%' }}
-          />
+          <div style={{ maxWidth: '800px', maxHeight: '400px' }}>
+            <img
+              src={project.finalMedia.src}
+              alt="Final media"
+              className="img-fluid mb-3 px-5 rounded"
+              style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+            />
+          </div>
         )}
       </div>
-      <div className="button-container d-flex justify-content-center pt-2 pb-5">
-        <a className="align-items-end mr-5 px-4" href={project.githubLink}>
-          <button type="button" className="btn custom-button">See on github ...</button>
+      <div className="container d-flex justify-content-center pt-5 pb-5">
+        <a className="align-items-end mr-5 px-4" target="_blank" href={project.githubLink}>
+          <button type="button" className="btn btn-outline-dark">See on github ...</button>
         </a>
-        <a className="align-items-end mr-5 px-4" href={project.documentationLink}>
-          <button type="button" className="btn custom-button">Documentation ...</button>
+        <a className="align-items-end mr-5 px-4" target="_blank" href={project.documentationLink}>
+          <button type="button" className="btn btn-outline-dark">Documentation ...</button>
         </a>
       </div>
     </div>
