4952a6c52b6e022d9cf9ae70b649f40c7fa53e99
SzewczykSzy <szewczyk.szy@gmail.com>
2024-09-05 14:08:49 +0200

    up

--- backend/app.py
+++ backend/app.py
@@ -15,8 +15,6 @@ CORS(app)
 GITHUB_USERNAME = 'SzewczykSzy'
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
-client = Client()
-
 MY_STRAVA_CLIENT_ID = os.getenv('STRAVA_CLIENT_ID')
 MY_STRAVA_CLIENT_SECRET = os.getenv('STRAVA_CLIENT_SECRET')
 ATHLETE_ID = 41343981
@@ -102,6 +100,8 @@ def fetch_github_commits_and_prs():
     }
 
 def fetch_strava_koms():
+    client = Client()
+    
     with open('strava/access_token.pickle', 'rb') as f:
         access_token = pickle.load(f)
 
@@ -140,6 +140,8 @@ def fetch_strava_koms():
 
 
 def fetch_strava_last_activity():
+    client = Client()
+
     with open('strava/access_token.pickle', 'rb') as f:
         access_token = pickle.load(f)
 
